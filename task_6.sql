1a526760be1c9f6444c4906210991ba8188ba297
j1ere <jeremywambua106@gmail.com>
2024-10-25 23:47:58 +0300

    coore

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,3 @@
--- Use the specified database
-USE alx_book_store;
-
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
