d1517b51247f1785ab3ce90b55f2f936b659f3d1
j1ere <jeremywambua106@gmail.com>
2024-10-25 23:52:15 +0300

    multiple insert cor

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,4 @@
--- Use the specified database
 USE alx_book_store;
-
--- Insert multiple rows into the Customers table
 INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
 (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
 (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
