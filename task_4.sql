851a8b093754b7999a9009a29e3a478268bb474d
j1ere <jeremywambua106@gmail.com>
2024-10-25 23:30:54 +0300

    decription print correction

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,7 @@
 -- Use the specified database
 USE alx_book_store;
 
--- Query the information_schema to get the full description of the books table
+-- Query the information_schema to get the full description of the Books table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
